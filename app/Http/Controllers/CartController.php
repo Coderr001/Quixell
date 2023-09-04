b0d74b4cc4954812a31a9cf45ab34418411b7dc1
michaelimmanuel <michael.mimo24@gmail.com>
2023-09-04 22:47:12 +0700

    added cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cart;
 
 class CartController extends Controller
 {
-    //
+    public function store(Request $request){
+        $cart = Cart::create([
+            'user_id' => auth()->user()->id,
+            'product_id' => $request->product_id,
+            'quantity' => $request->quantity ?? 1
+        ]);
+        return response()->json([
+            'message' => 'Product added to cart successfully',
+            'cart' => $cart,
+        ], 201);
+    }
+
+    public function getByUser(){
+        $cart = Cart::where('user_id', auth()->user()->id)->get();
+        return response()->json([
+            'cart' => $cart
+        ], 200);
+    }
+
+    public function checkOut(){
+        $cart = Cart::where('user_id', auth()->user()->id)->get();
+        foreach($cart as $item){
+            $item->delete();
+        }
+        return response()->json([
+            'message' => 'Checkout successful'
+        ], 200);
+    }
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\Product;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // seed product table with 10 products
+        $products = [
+            [
+                'name' => 'Product 1',
+                'description' => 'Description of Product 1',
+                'price' => '19.99',
+                'image' => 'product1.jpg',
+            ],
+            [
+                'name' => 'Product 2',
+                'description' => 'Description of Product 2',
+                'price' => '29.99',
+                'image' => 'product2.jpg',
+            ],
+            // Add more product data as needed
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Insert the data into the 'products' table
+        foreach ($products as $productData) {
+            Product::create($productData);
+        }
+
     }
 }
